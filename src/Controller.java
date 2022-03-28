6f286a1e7947e49a059fc745351d3e508e7366ef
TTmesb <emil.aberg@elev.ga.ntig.se>
2022-03-28 09:40:41 +0200

    Initial commit

---
+++ src/Controller.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+/**
+ * Skapat mars 2022-03-28 - 08:48
+ * Main
+ * Skriven av coolingen
+ */
+public class Controller {
+    public static void main(String[] args) {
+
+
+        String[] options = {"Client", "Server"};
+        //Integer[] options = {1, 3, 5, 7, 9, 11};
+        //Double[] options = {3.141, 1.618};
+        //Character[] options = {'a', 'b', 'c', 'd'};
+        int x = JOptionPane.showOptionDialog(null, "Vill du köra som Server eller Client",
+                "Välj",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        if (x == 1) {
+        View v = new View();
+        }else {
+           Model m = new Model();
+        }
+    }
+
+}
---
+++ src/Model.java
@@ -0,0 +1,7 @@
+/**
+    * Skapat mars 2022-03-28 - 08:48
+    * Model
+    * Skriven av coolingen
+    */
+public class Model {
+}
