4dd671e740cd2d7fe5420b5f330f244dbdcdbce4
TTmesb <emil.aberg@elev.ga.ntig.se>
2022-03-31 13:41:53 +0200

    :(

--- src/Controller.java
+++ src/Controller.java
@@ -6,6 +6,7 @@ import javax.swing.*;
  * Skriven av coolingen
  */
 public class Controller {
+
     public static void main(String[] args) {
 
 
@@ -13,13 +14,15 @@ public class Controller {
         //Integer[] options = {1, 3, 5, 7, 9, 11};
         //Double[] options = {3.141, 1.618};
         //Character[] options = {'a', 'b', 'c', 'd'};
-        int x = JOptionPane.showOptionDialog(null, "Vill du köra som Server eller Client",
+        int x = JOptionPane.showOptionDialog(null, "Vill du köra som Server eller Client?",
                 "Välj",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-        if (x == 1) {
+        if (x == 0) {
         View v = new View();
         }else {
            Model m = new Model();
+
+
         }
     }
 
--- src/Model.java
+++ src/Model.java
@@ -1,7 +1,27 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
     * Skapat mars 2022-03-28 - 08:48
     * Model
     * Skriven av coolingen
     */
 public class Model {
+    public void Server() throws IOException {
+        int port = 3444;
+        ServerSocket serverSocket;
+        Socket socket;
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("Waiting for connections!");
+                socket = serverSocket.accept();
+            }
+
+            } catch(Exception e){
+                System.out.println("Server fail");
+            }
+        }
+
 }
